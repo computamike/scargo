e24bee84c74be56d530df716da47e45a37f111f9
Mike Hingley <mike@titaniumbunker.com>
2015-10-24 16:55:38 +0100

    update

--- utilities/fixer.py
+++ utilities/fixer.py
@@ -15,7 +15,7 @@
 
 #You should have received a copy of the GNU General Public License along
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
-# Update 1..2..3
+# Update 1..2..3..4
 
 import argparse
 from lxml import etree as ET
